30b189620f636133536f1308ee07306eaf6c6a27
Tural Nasirov <tural.nasirov@hotmail.com>
2021-05-12 18:03:44 +0200

    Create S3 bucket

--- lib/aws-cdk-stack.ts
+++ lib/aws-cdk-stack.ts
@@ -1,9 +1,16 @@
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 
 export class AwsCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, 'simpleAppBucketTural', {
+      encryption: BucketEncryption.S3_MANAGED,
+    });
+    new cdk.CfnOutput(this, 'simpleAppBucketTuralOutput', {
+      value: bucket.bucketName,
+      exportName: 'simpleAppBucketTural',
+    });
   }
 }
--- test/aws-cdk.test.ts
+++ test/aws-cdk.test.ts
@@ -1,13 +1,51 @@
+
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as AwsCdk from '../lib/aws-cdk-stack';
+import '@aws-cdk/assert/jest';
 
-test('Empty Stack', () => {
+test('Simpe App Stack', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new AwsCdk.AwsCdkStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
-      "Resources": {}
+      "Resources": {
+        "simpleAppBucketTuralE81910B4": {
+          "Type": "AWS::S3::Bucket",
+          "Properties": {
+            "BucketEncryption": {
+              "ServerSideEncryptionConfiguration": [
+                {
+                  "ServerSideEncryptionByDefault": {
+                    "SSEAlgorithm": "AES256"
+                  }
+                }
+              ]
+            }
+          },
+          "UpdateReplacePolicy": "Retain",
+          "DeletionPolicy": "Retain",
+        },
+      },
+      "Outputs": {
+        "simpleAppBucketTuralOutput": {
+          "Value": {
+            "Ref": "simpleAppBucketTuralE81910B4"
+          },
+          "Export": {
+            "Name": "simpleAppBucketTural"
+          }
+        }
+      },
     }, MatchStyle.EXACT))
+});
+
+test('Stack create a S3 bucket', () => {
+  // ARRANGE
+  const app = new cdk.App();
+  // ACT
+  const stack = new AwsCdk.AwsCdkStack(app, 'MyTestStack');
+  // ASSERT
+  expect(stack).toHaveResource('AWS::S3::Bucket');
 });
